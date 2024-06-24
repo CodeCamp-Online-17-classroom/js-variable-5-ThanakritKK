dfbc7fb740a672dddfcac2a65083f49755d3496b
ThanakritKK <xseigh.xc@gmail.com>
2024-06-24 20:54:52 +0700

    add bio

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // write code here
+let Name = "Thanakrit";
+let lastname = "Kaewsringam";
+let age = "23";
+let address = "777 Tokyo,Japan";
+
+console.log("Full name : " + Name + " " + lastname + ", Age:" + age + ", Address:" + address );
